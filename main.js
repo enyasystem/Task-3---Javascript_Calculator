6ca1a26ee3090ff5192c525de853020bf5d03af7
enyasystem <enyaelvis@gmail.com~>
2022-05-03 13:57:09 +0100

    When a user hits the decimal key, it should appear then, then if the user hits a no. before the decimal then it should be appended to the dispay as well

--- main.js
+++ main.js
@@ -58,3 +58,18 @@ if (!action) {
         display.textContent = keyContent
     }    
 }
+
+//If Calculator shows a non-zero number, then let the number be appended to the clicked key to the display number: to append a number: a string will be concatenated
+if (!action) {
+    if (displayedNum === '0') {
+        display.textContent = keyContent
+    } else {
+        display.textContent = displayedNum + keyContent
+    }
+}
+
+//When a user hits the decimal key, it should appear then, then if the user hits a no. before the decimal then it should be appended to the dispay as well
+//To do that, the . (decimal will be a concatenate to the display number
+if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+}
