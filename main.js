9946a3fefea4001278ea58a63e746d755394002f
enyasystem <enyaelvis@gmail.com~>
2022-05-03 13:24:49 +0100

    Grid settings adjusted

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Calculator-Keys
-let calculator = document.querySelector('.calculator')
-let keys = calculator.querySelector('.calculator-keys')
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator-keys')
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')){
@@ -9,8 +9,8 @@ keys.addEventListener('click', e => {
 })
 
 //Action attribute to determine the key pressed
-let key = e.target
-let action = key.dataset.action
+const key = e.target
+const action = key.dataset.action
 
 //If key pressed does not have an data-action attribute, then it must be a number
 if (!action) {
